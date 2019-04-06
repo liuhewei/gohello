0bfe7aeb23fc927ec801d31e6b960e9af25f67ad
l00431346 <liuhewei@huawei.com>
2019-04-06 20:43:17 +0800

    Change the dead-loop to http server

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
-func main(){
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Hello World!</h1>"+"<div>Path: "+r.URL.Path+"</div>")
+	log.Printf("Visit Path: %s\n", r.URL.Path)
+}
 
-  for{
-    fmt.Println("hello world")
-    time.Sleep(1 * time.Second)
-  }
-  
+func main() {
+
+	// for {
+	// 	fmt.Println("hello world")
+	// 	time.Sleep(1 * time.Second)
+	// }
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
